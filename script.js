be4e2afcfcb3794f282e2e09aee7ac6cd765485d
Juan Jose <juanjo.cardan05@gmail.com>
2025-06-04 22:26:46 -0500

    codigo funcional parcial

--- script.js
+++ script.js
@@ -4,32 +4,31 @@ function loadCareer(career) {
 
   if (career === "civil") { 
     modalBody.innerHTML = `
-        <h3>Ingeniería Civil</h3>
-        <div class="malla-container">
-            <img src="assets/civil_malla.jpg" alt="Mapa Ingeniería Civil">
-            
-            <!-- Materias clickeables con coordenadas aproximadas -->
-            <div class="materia" style="top: 65px; left: 90px;" data-course="Cálculo Diferencial"></div>
-            <div class="materia" style="top: 125px; left: 90px;" data-course="Geometría"></div>
-            <div class="materia" style="top: 185px; left: 90px;" data-course="Biología I"></div>
-            <div class="materia" style="top: 245px; left: 90px;" data-course="Química General e Inorgánica"></div>
-
-        </div>
-
-
+      <h3>Ingeniería Civil</h3>
+      <div class="malla-container">
+        <img src="assets/civil_malla.jpg" alt="Mapa Ingeniería Civil">
+        
+        <!-- Materias clickeables con coordenadas aproximadas -->
+        <div class="materia" style="top: 115px; left: 15px;" data-course="Cálculo Diferencial"></div>
+        <div class="materia" style="top: 205px; left: 15px;" data-course="Geometría"></div>
+        <div class="materia" style="top: 295px; left: 15px;" data-course="Biología I"></div>
+        <div class="materia" style="top: 383px; left: 15px;" data-course="Química General e Inorgánica"></div>
+        <div class="materia" style="top: 472px; left: 15px;" data-course="Fundamentos de programación"></div>
+        <div class="materia" style="top: 650px; left: 15px;" data-course="Comunicación y Ciencia"></div>
+        <div class="materia" style="top: 740px; left: 15px;" data-course="Introducción a la Ingeniería"></div>
+      </div>
     `;
-    // Y activa los clics:
+
+    // Activar clics en las materias
     setTimeout(() => {
-    document.querySelectorAll('.materia').forEach(materia => {
+      document.querySelectorAll('.materia').forEach(materia => {
         materia.addEventListener('click', () => {
-            alert(`Has hecho clic en: ${div.dataset.materia}`);
-            });
+          alert(`Has hecho clic en: ${materia.dataset.course}`);
         });
+      });
     }, 0);
-    }
-    modal.style.display = "block";
-}
-  /*else if (career === "sistemas") {
+  }
+  else if (career === "sistemas") {
     modalBody.innerHTML = `
       <h3>Ingeniería de Sistemas</h3>
       <p>Contenido del mapa curricular próximamente...</p>
@@ -38,10 +37,11 @@ function loadCareer(career) {
     modalBody.innerHTML = "";
   }
 
+  // Mostrar el modal
   modal.style.display = "block";
 }
 
 function closeModal() {
   const modal = document.getElementById("careerModal");
   modal.style.display = "none";
-}*/
+}
