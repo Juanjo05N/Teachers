1e911722301bf91b8bd3dc9d0f5128317b5aec67
Juan Jose <juanjo.cardan05@gmail.com>
2025-06-04 21:54:09 -0500

    `Added interactive map to civil engineering career modal and updated styles`

--- script.js
+++ script.js
@@ -2,12 +2,34 @@ function loadCareer(career) {
   const modal = document.getElementById("careerModal");
   const modalBody = document.getElementById("modalBody");
 
-  if (career === "civil") {
+  if (career === "civil") { 
     modalBody.innerHTML = `
-      <h3>Ingeniería Civil</h3>
-      <img src="assets/civil_malla.jpg" alt="Mapa Ingeniería Civil" style="max-width:100%; height:auto;">
+        <h3>Ingeniería Civil</h3>
+        <div class="malla-container">
+            <img src="assets/civil_malla.jpg" alt="Mapa Ingeniería Civil">
+            
+            <!-- Materias clickeables con coordenadas aproximadas -->
+            <div class="materia" style="top: 65px; left: 90px;" data-course="Cálculo Diferencial"></div>
+            <div class="materia" style="top: 125px; left: 90px;" data-course="Geometría"></div>
+            <div class="materia" style="top: 185px; left: 90px;" data-course="Biología I"></div>
+            <div class="materia" style="top: 245px; left: 90px;" data-course="Química General e Inorgánica"></div>
+
+        </div>
+
+
     `;
-  } else if (career === "sistemas") {
+    // Y activa los clics:
+    setTimeout(() => {
+    document.querySelectorAll('.materia').forEach(materia => {
+        materia.addEventListener('click', () => {
+            alert(`Has hecho clic en: ${div.dataset.materia}`);
+            });
+        });
+    }, 0);
+    }
+    modal.style.display = "block";
+}
+  /*else if (career === "sistemas") {
     modalBody.innerHTML = `
       <h3>Ingeniería de Sistemas</h3>
       <p>Contenido del mapa curricular próximamente...</p>
@@ -22,4 +44,4 @@ function loadCareer(career) {
 function closeModal() {
   const modal = document.getElementById("careerModal");
   modal.style.display = "none";
-}
+}*/
